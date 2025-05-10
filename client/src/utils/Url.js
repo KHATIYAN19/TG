962fa91c1406fbdc2169434dee957118ff595a1f
LAVI KHATIYAN <lavikhatiyan43177@gmail.com>
2025-05-10 12:57:04 +0530

    base url change

--- client/src/utils/Url.js
+++ client/src/utils/Url.js
@@ -1,5 +1,5 @@
 
-//const BASE_URL = 'https://targettrek.onrender.com';
+const BASE_URL = 'https://targettrek.onrender.com';
 
-const BASE_URL="http://localhost:5001";
+// const BASE_URL="http://localhost:5001";
 export default BASE_URL;
