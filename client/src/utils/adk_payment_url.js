e7a1ee74b69a8fc738f4972f91fdffa937d1c8cc
Lavi Choudhary <lavichoudhary@Lavis-MacBook-Air.local>
2026-09-19 14:55:49 +0530

    changed temp payment url

--- client/src/utils/adk_payment_url.js
+++ client/src/utils/adk_payment_url.js
@@ -1,2 +1,2 @@
-const ADK_PAYMENT_URL="https://dashboard-staging.payu.in/web/A8FF5BA5419E53758915A2BCC2D0BB25";
+const ADK_PAYMENT_URL="https://u.payu.in/Er92WjaNxBYF";
 export default ADK_PAYMENT_URL;
